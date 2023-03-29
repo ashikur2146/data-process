c51d6fe6f5552c99f73fb43c59f6e9151724bffe
mohammad.ashikur <mohammad.ashikur@neural-semiconductor.com>
2023-03-29 10:47:29 +0600

    Project setup and initial commit

---
+++ DATA-PROCESSING/src/main/java/data/processing/Main.java
@@ -0,0 +1,11 @@
+package data.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
